c609549d274686ba4cb5e89114d72ad625b7fa54
TheAniketMauryaManjeya <mauryavanshi72@gmail.com>
2025-08-22 23:04:22 +0530

    02 - customization

--- multitenant-ecommerce/src/app/page.tsx
+++ multitenant-ecommerce/src/app/page.tsx
@@ -1,12 +1,30 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@radix-ui/react-checkbox";
+import { Progress } from "@radix-ui/react-progress";
 
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500">
-        Hello world
-      </p>
+    <div className="flex flex-col gap-y-4">
+      <div className="p-4">
+        <Button variant="elevated">
+          I am a button 
+        </Button>
+      </div>
+      <div>
+        <Input placeholder="I am an input" />
+      </div>
+      <div>
+        <Progress value={50}/>
+      </div>
+      <div>
+        <Textarea placeholder="This is a textarea" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
-  );
+  )
 }
